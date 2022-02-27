95a30bcdff912a0a40ace96624c7e2bbcecd5eab
Julio Hernandez <jahp8719@gmail.com>
2022-02-27 15:40:39 -0500

    fix: deleting repo

--- church-api/src/services/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './file-upload.service';
-export * from './hash-password.service';
-export * from './jwt.service';
-export * from './user.service';
-export * from './validator.service';
-
-export * from './email.service';
